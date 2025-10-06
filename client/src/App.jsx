30922f3304276a8520d81aa70cfb8c25cb196f4c
Sahil Jaiswal <jaiswalsahil7741@gmail.com>
2025-10-06 05:55:55 +0530

    refactor: streamline frontend shell and assets

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar.jsx'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import Home from './pages/Home.jsx'
 import Users from './pages/Users.jsx'
 import { ToastContainer } from 'react-toastify'
 import Transactions from './pages/Transactions.jsx'
@@ -15,7 +11,7 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Users/>} />
         <Route path="/users" element={<Users/>} />
         <Route path="/transactions" element={<Transactions />} />
         {/* <Route path="/relationships" element={<Relationships />} /> */}
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,14 +3,13 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav className="glass w-full p-4 flex justify-between text-white text-[16px]">
-        <div className="navbar__logo heading-neon text-xl">
-            <Link to="/">Graph Explorer</Link>
+    <nav className="glass w-full px-6 py-4 flex items-center justify-between text-slate-100 text-[15px] border border-cyan-200/10 shadow-lg shadow-cyan-500/10">
+        <div className="navbar__logo heading-neon text-2xl tracking-tight">
+            <Link to="/users">Graph Explorer</Link>
         </div>
-        <ul className="navbar__links flex gap-4">
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/users">Users</Link></li>
-            <li><Link to="/transactions">Transactions</Link></li>
+        <ul className="navbar__links flex gap-6">
+            <li><Link className="hover:text-cyan-300 transition-colors" to="/users">Users</Link></li>
+            <li><Link className="hover:text-cyan-300 transition-colors" to="/transactions">Transactions</Link></li>
             {/* <li><Link to="/relationships">Relationships</Link></li> */}
         </ul>
     </nav>    ) 
